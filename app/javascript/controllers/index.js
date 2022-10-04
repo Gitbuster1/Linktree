d227280d17b474ac98b6d974208dc3688bce3415
Gitbuster1 <tiberiug2010@gmail.com>
2022-10-04 19:16:51 +0300

    Add share link option to profile page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,6 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
+import Clipboard from "stimulus-clipboard";
 application.register("hello", HelloController)
+application.register('clipboard', Clipboard)
